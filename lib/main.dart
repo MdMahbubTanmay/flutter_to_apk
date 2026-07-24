3da19a262304173fef6a7ec5ace452a0660de728
MdMahbubTanmay <m19212628@gmail.com>
2026-07-24 23:49:25 +0600

    Added Curved Stylish Container

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,62 @@ class MyApp extends StatelessWidget{
 
       home: Scaffold(
 
+
+        backgroundColor :const Color(0xFF0D0B14),
+
         appBar: AppBar(
-          title: Text('Test On Android'),
+          title: Text(
+            'Test On Android',
+            style: TextStyle(color: Colors.blueGrey),
+          ),
           centerTitle: true,
+          backgroundColor: const Color(0xFF151224),
 
 
         ),
+
+        body :
+        
+        Container(
+          
+          padding: EdgeInsets.all(15),
+
+            
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+            Container(
+              
+              
+              width: 200,
+              height: 100,
+              decoration: BoxDecoration(
+
+              color: const Color(0xff161224),
+                borderRadius: BorderRadius.circular(25),
+
+                border: Border.all(
+                  color: const Color(0xff3b2d54)
+                )
+              ),
+              child:Center(
+              child: 
+              
+              Text(
+                
+                'data', style: TextStyle(color: Color(0xffffffff)),),),
+
+            ),
+
+          ],),
+       
+        ),
+      
+
 
       )
       
